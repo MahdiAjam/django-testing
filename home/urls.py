8c94b5991a2941e5a5920617ffd42dff817458eb
MahdiAjam <mahdiajam8754@gmail.com>
2025-03-01 17:47:01 +0330

    added registration

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,5 @@ app_name = 'home'
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('about/<str:username>/', views.AboutView.as_view(), name='about'),
+    path('register/', views.UserRegisterView.as_view(), name='register'),
 ]
