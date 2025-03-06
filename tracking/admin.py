abfe28ef971299790996420aa3b2c3f0897d745e
MahdiAjam <mahdiajam8754@gmail.com>
2025-03-06 17:24:43 +0330

    added the model for api request loging

--- tracking/admin.py
+++ tracking/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import APIRequestLog
 
-# Register your models here.
+class APIRequestLogAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'request_at',
+        'response_ms',
+        'status_code',
+        'user',
+        'view_method',
+        'path',
+        'remote_address',
+        'host',
+        'query_params',
+    )
+
+
+admin.site.register(APIRequestLog, APIRequestLogAdmin)
