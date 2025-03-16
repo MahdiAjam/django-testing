bbfbf99d0b8cd7806f1bcdb21022be295fd2fefd
MahdiAjam <mahdiajam8754@gmail.com>
2025-03-16 16:35:38 +0330

    making new views and urls for testing

--- tracking/tests/urls.py
+++ tracking/tests/urls.py
@@ -4,4 +4,10 @@ from . import views as test_views
 urlpatterns = [
     path('no-logging/', test_views.MockNoLoggingView.as_view()),
     path('logging/', test_views.MockLoggingView.as_view()),
+    path('explicit-logging/', test_views.MockExplicitLoggingView.as_view()),
+    path('custom-check-logging/', test_views.MockCustomCheckLoggingView.as_view()),
+    path('session-auht-logging/', test_views.MockSessionAuthLoggingView.as_view()),
+    path('token-auth-logging/', test_views.MockTokenAuthLoggingView.as_view()),
+    path('sensitive-fields-logging/', test_views.MockSensitiveFieldsLoggingView.as_view()),
+    path('invalid-cleaned-substitute-logging/', test_views.MockInvalidCleanedSubstituteLoggingView.as_view()),
 ]
--- tracking/tests/views.py
+++ tracking/tests/views.py
@@ -1,6 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from tracking.mixins import LoggingMixins
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
 
 
 class MockNoLoggingView(APIView):
@@ -11,3 +13,52 @@ class MockNoLoggingView(APIView):
 class MockLoggingView(LoggingMixins, APIView):
     def get(self, request):
         return Response('With logging')
+
+
+class MockExplicitLoggingView(LoggingMixins, APIView):
+    logging_methods = ['POST']
+
+    def get(self, request):
+        return Response('No logging')
+
+    def post(self, request):
+        return Response('With logging')
+
+
+class MockCustomCheckLoggingView(LoggingMixins, APIView):
+    def should_log(self, request, response):
+        return 'log' in response.data
+
+    def get(self, request):
+        return Response('With logging')
+
+    def post(self, request):
+        return Response('No recording')
+
+
+class MockSessionAuthLoggingView(LoggingMixins, APIView):
+    authentication_classes = (SessionAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        return Response('With session auth logging')
+
+
+class MockTokenAuthLoggingView(LoggingMixins, APIView):
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, reqeust):
+        return Response('With token auth logging')
+
+
+class MockSensitiveFieldsLoggingView(LoggingMixins, APIView):
+    sensitive_fields = {'mY_fiElD'}
+
+    def get(self, request):
+        return Response('With logging')
+
+
+class MockInvalidCleanedSubstituteLoggingView(LoggingMixins, APIView):
+    CLEANED_SUBSTITUTE = 1
+
